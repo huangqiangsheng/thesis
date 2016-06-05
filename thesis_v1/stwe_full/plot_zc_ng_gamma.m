87e235d950716e32a054c6a0bf6b7dd5f5864ae8
tiaotu <qiangsheng.huang@foxmail.com>
2016-06-05 20:15:50 +0800

    after anonymity reviewer

--- thesis_v1/stwe_full/plot_zc_ng_gamma.m
+++ thesis_v1/stwe_full/plot_zc_ng_gamma.m
@@ -1,4 +1,4 @@
-run ucsb_ms_chapt2
+run ucsb_chapt2
 freq = active_data(:,1);
 Zc = active_data(:,2);
 loss = active_data(:,4);
@@ -11,8 +11,19 @@ xlabel('f (GHz)');
 ylabel('Characteristic Impedance Z_C(\Omega)');
 axis tight
 figure(2)
-plot(freq,loss); hold on;
-plot(freq,real(neff));
+[AX,H1,H2] = plotyy(freq,loss,freq,real(neff)); hold on;
+HH1=get(AX(1),'Ylabel');
+set(HH1,'String','Loss (dB/mm)');
+set(HH1,'Color','k');
+HH2=get(AX(2),'Ylabel');
+set(HH2,'String','n_m');
+set(HH2,'Color','k');
+% set(H1,'Color','k');
+% set(H2,'Color','k');
+% set(AX(1),'XLim',[0 100]);
+% set(AX(2),'XLim',[0]);
+% set(AX(1),'xtick',0:10:100);
+% set(AX(2),'xtick',0);
 hold off;
 xlabel('f (GHz)');
-axis tight
+% axis tight
